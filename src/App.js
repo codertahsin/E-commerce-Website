312ae43f0cd6c0207c8a3c10af6704836f154f01
codertahsin <ahnaftahsin11@gmail.com>
2022-09-24 20:54:07 +0600

    prtoject file

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import Header from './Components/Header/Header';
+import Banner from './Components/Banner/Banner';
+import Service from './Components/Service/Service';
+import About from './Components/About/About';
+import Shop from './Components/Shop/Shop';
+import Discover from './Components/Discover/Discover';
+import Testimonial from './Components/Testimonial/Testimonial';
+import Newsletter from './Components/Newsletter/Newsletter';
+import Footer from './Components/Footer/Footer';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+// import LoginPage from './Components/Pages/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Banner />
+      <Service />
+      <About />
+      <Shop />
+      <Discover />
+      <Testimonial />
+      <Newsletter />
+      <Footer />
+      {/* <LoginPage /> */}
+    </div>
+  );
+}
+
+export default App;
